32348e996776ace7637c881c40a7f3661cda361e
johan garcia <garciamartinezjohanalexander@gmail.com>
2024-01-21 23:18:23 -0600

    Primera Version

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] prueba){
+        Mascota masc1 = new Mascota ();
+        masc1.nombre="Merida";
+        masc1.edad = 4;
+        masc1.saludar();
+        Mascota masc2=new Mascota();
+        masc2.nombre="Yvett";
+        // masc2.edad=2;
+        masc2.saludar();
+    }
+}
---
+++ Mascota.java
@@ -0,0 +1,11 @@
+public class Mascota{
+    
+    String nombre,color,textura;
+    float tamano,peso;
+    byte edad;
+
+ public void saludar(){
+ System.out.println("hola, soy "+nombre+" tengo "+edad+" años" +" mi color es: "+color);
+}
+    
+}
